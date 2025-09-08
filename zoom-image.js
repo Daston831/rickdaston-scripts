e84c15fe62383a85907d02c616effa5003faba37
Daston831 <daston831@gmail.com>
2025-09-08 10:46:20 -0400

    Update zoom-image.js

--- zoom-image.js
+++ zoom-image.js
@@ -15,8 +15,8 @@
     image.style.objectFit = "cover";
     image.style.transformOrigin = "bottom right";
 
-    // Initial zoom for 65% visibility
-    let scale = 1.25; // 65% visible
+    // Initial zoom for 50% visibility
+    let scale = 1.25; // 50% visible
     let offsetX = 0, offsetY = 0;
     let lastX = 0, lastY = 0;
     let startDist = 0;
